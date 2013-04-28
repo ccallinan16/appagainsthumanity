10c090ee6d170a36ef48d7cd8cd532eff255ca79
pwkoch <pkoch@student.tugraz.at>
2013-04-28 11:34:38 +0200

    tested list layouts
    added linear and list layout to main activity
    added createGame activity

--- src/at/tugraz/iicm/ma/appagainsthumanity/MainActivity.java
+++ src/at/tugraz/iicm/ma/appagainsthumanity/MainActivity.java
@@ -2,14 +2,28 @@ package at.tugraz.iicm.ma.appagainsthumanity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	private ListView gameListView;
+	private ArrayAdapter<String> gameArrayAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		String[] stringarray = {"test1", "test2", "test3"};
+		gameListView = (ListView) findViewById(R.id.game_list_view);
+		
+		
+		gameArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stringarray);
+		gameListView.setAdapter(gameArrayAdapter);
 	}
 
 	@Override
@@ -18,5 +32,13 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+    public void createGame(View view) {
+    	Intent intent = new Intent(this, CreateGameActivity.class);
+//    	EditText editText = (EditText) findViewById(R.id.edit_message);
+//    	String message = editText.getText().toString();
+//    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    }
 
 }
