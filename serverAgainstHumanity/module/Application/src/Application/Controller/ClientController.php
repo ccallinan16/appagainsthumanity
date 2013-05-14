d9fbf2969b4d8293ae0689f908a31782cd1de957
pwkoch <pkoch@student.tugraz.at>
2013-05-14 11:00:15 +0200

    added server project files

---
+++ serverAgainstHumanity/module/Application/src/Application/Controller/ClientController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Zend\XmlRpc\Client;
+
+class ClientController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    	$client = new Client('http://localhost/');
+        try {
+            $data = $client->call('cf.test');
+            $this->layout( 'layout/xml-layout' );
+            
+			return new ViewModel(array(
+            	'data' => $data
+        	));
+
+        } catch (Zend_XmlRpc_Client_HttpException $e) {
+            require_once 'Zend/Exception.php';
+            throw new Zend_Exception($e);
+        } catch (Zend_XmlRpc_Client_FaultException $e) {
+            require_once 'Zend/Exception.php';
+                throw new Zend_Exception($e);
+        }
+    }
+    
+}
