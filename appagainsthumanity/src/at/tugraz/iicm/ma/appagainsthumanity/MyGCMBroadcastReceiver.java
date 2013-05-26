dc8acee38156bb0d7b7de2f30fee80d758e0e190
Elisabeth Getzner <elisabeth.getzner@student.tugraz.at>
2013-05-26 18:40:29 +0200

    pt 2

---
+++ appagainsthumanity/src/at/tugraz/iicm/ma/appagainsthumanity/MyGCMBroadcastReceiver.java
@@ -0,0 +1,66 @@
+package at.tugraz.iicm.ma.appagainsthumanity;
+
+import org.gcm.trials.CommonUtilities;
+import org.gcm.trials.WakeLocker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.widget.Toast;
+import at.tugraz.iicm.ma.appagainsthumanity.connection.NotificationHandler;
+import at.tugraz.iicm.ma.appagainsthumanity.db.DBProxy;
+
+
+public class MyGCMBroadcastReceiver extends BroadcastReceiver {
+	
+	protected static final String KEY_NOTIFICATION_TYPE = "notification_type";
+	protected static final String KEY_NOTIFICATION_ID = "notification_id";
+	UpdateTask	updateTask;
+
+	public MyGCMBroadcastReceiver(DBProxy proxy) {
+		updateTask = new UpdateTask(proxy);
+
+	}
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+		System.err.println("BroadcastReceiver CALLED!!");
+		String newMessage = intent.getExtras().getString(CommonUtilities.KEY_MESSAGE);
+		// Waking up mobile if it is sleeping
+		WakeLocker.acquire(context.getApplicationContext());
+		
+		updateTask.execute();
+		
+		// Showing received message
+//		lblMessage.append(newMessage + "\n");			
+		Toast.makeText(context.getApplicationContext(), "New Message: " + newMessage, Toast.LENGTH_LONG).show();
+		
+		// Releasing wake lock
+		WakeLocker.release();
+	}
+	
+	private class UpdateTask extends AsyncTask <Void,Void,Void>{
+
+		private NotificationHandler handler;
+
+		public UpdateTask(DBProxy proxy) {
+			this.handler = new NotificationHandler(proxy);
+		}
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+
+			handler.checkAndHandleUpdates();
+			
+			//int type = intent.getExtras().getInt(KEY_NOTIFICATION_TYPE);
+			//int notificationID = intent.getExtras().getInt(KEY_NOTIFICATION_ID);
+			//handler.handleUpdate(type,notificationID);
+
+			
+			return null;
+		}
+	}
+
+}
---
+++ serverAgainstHumanity/module/Application/src/Application/Controller/ServerController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Application\Model\Rpc;
+use Application\Model\Gcm;
+use Application\Model\Notification;
+use Application\Model\NotificationHandler;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Zend\XmlRpc\Server;
+define("GOOGLE_API_KEY", "AIzaSyClphHWMig6AY_bSun4RuWgVO3tAK5SYTg");
+
+class ServerController extends AbstractActionController
+{
+	const NAMESPACE_RPC           = "aah";
+	const NAMESPACE_NOTIFICATION  = "notification";
+
+	public function indexAction()
+	{
+		$server = new Server();
+		//$gcm = new GCM();
+		$rpc = new Rpc($this->getServiceLocator());
+		$server->setClass($rpc, IndexController::NAMESPACE_RPC);
+		$notificationHandler = new NotificationHandler($this->getServiceLocator());
+		$server->setClass($notificationHandler, IndexController::NAMESPACE_NOTIFICATION);
+			
+		$uid = $rpc->getUserId("getzner.e@googlemail.com");
+
+		$rpc->sendNotification(Notification::notification_new_game,$uid,1);
+		
+		echo $server->handle();
+
+		return $this->getResponse();
+	}
+
+}
